ebf960002abe7d1ce5ca6fb22bbb6b677cecb846
annietberry <tombariannie@gmail.com>
2021-05-17 13:31:45 -0700

    Added imports to main.js

--- app/main.js
+++ app/main.js
@@ -1,3 +1,10 @@
 import Alligator from "./models/Alligator.js";
+import Bird from "./models/Alligator.js"
+import Dog from "./models/Alligator.js"
+import Bird from "./models/Alligator.js"
+import Eagle from "./models/Alligator.js"
 
-let ally = new Alligator("Ally", "Green", "F", 200, 60, "big");
+let ally = new Alligator("Ally", "green", "F", 200, 60, "big");
+let charlie = new Bird("charlie", "blue", "F", 3, 0, "small");
+let houston = new Dog("Houston", "brown", "male", 50, 0, "small");
+let beady = new Eagle("Beady", "gold", "male", 30, 0, "small")
--- app/models/Alligator.js
+++ app/models/Alligator.js
@@ -1,23 +1,114 @@
 class Alligator {
     constructor(
-        name = '',
-        color = '',
-        gender = '',
-        weight = 0,
-        tailLengthInch = 0,
-        teethSize = '',
+        name = 'Bob',
+        color = 'green',
+        gender = 'Male',
+        weight = 200,
+        tailLengthInch = 36,
+        teethSize = 'medium',
     ) {
         this.id = 0,
-        this.name = name;
+            this.name = name;
         this.color = color;
         this.gender = gender;
         this.weight = weight;
         this.tailLengthInch = tailLengthInch;
         this.teethSize = teethSize;
     }
-    eat(food){
+    eat(food) {
         console.log(`Chomp chomp all the ${food}ies.`)
     }
 }
 
+class Bird {
+    constructor(
+        name = 'Coco',
+        color = 'orange',
+        gender = 'F',
+        weight = 1,
+        beakSize = 'small',
+    ) {
+        this.id = 0,
+            this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.beakSize = beakSize;
+    }
+    eat(food) {
+        console.log(`Chomp chomp all the ${food}ies.`)
+    }
+}
+
+class Dog {
+    constructor(
+        name = 'Zed',
+        color = 'black',
+        gender = 'Male',
+        weight = 100,
+        tailLengthInch = 24,
+        pawSize = 'big',
+    ) {
+        this.id = 0,
+            this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.tailLengthInch = tailLengthInch;
+        this.pawSize = pawSize;
+    }
+    eat(food) {
+        console.log(`Chomp chomp all the ${food}ies.`)
+    }
+}
+
+class Eagle {
+    constructor(
+        name = 'Merica',
+        color = 'white',
+        gender = 'Male',
+        weight = 20,
+        beakSize = 'big',
+    ) {
+        this.id = 0,
+            this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.beakSize = beakSize;
+    }
+    eat(food) {
+        console.log(`Chomp chomp all the ${food}ies.`)
+    }
+}
+
+class Panda {
+    constructor(
+        name = 'Fluffy',
+        color = 'red',
+        gender = 'Female',
+        weight = 400,
+        gutSize = 'big',
+    ) {
+        this.id = 0,
+            this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.gutSize = gutSize;
+    }
+    eat(food) {
+        console.log(`Chomp chomp all the ${food}ies.`)
+    }
+}
+
+let ally = new Alligator("Ally", "Green", "F", 200, 60, "big");
+let Billy = new Bird("Billy", "purple", "M", 2, "big");
+let Sam = new Panda("Sam", "white", "Male", 200, "Medium")
+let Mini = new Dog("Mini", "white", "Female", 40,)
+
 export default Alligator;
+export default Dog;
+export default Panda;
+
+export default Bird;
