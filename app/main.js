624736791b4ccce664b4c44599befb0a4d81a586
annietberry <tombariannie@gmail.com>
2021-05-17 13:34:00 -0700

    imports

--- app/main.js
+++ app/main.js
@@ -3,8 +3,11 @@ import Bird from "./models/Alligator.js"
 import Dog from "./models/Alligator.js"
 import Bird from "./models/Alligator.js"
 import Eagle from "./models/Alligator.js"
+import Panda from "./models/Alligator.js"
+
 
 let ally = new Alligator("Ally", "green", "F", 200, 60, "big");
 let charlie = new Bird("charlie", "blue", "F", 3, 0, "small");
 let houston = new Dog("Houston", "brown", "male", 50, 0, "small");
 let beady = new Eagle("Beady", "gold", "male", 30, 0, "small")
+let frank = new Panda("Frank", "white", "male", "200", 0, "small")
